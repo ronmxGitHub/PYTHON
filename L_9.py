383f7c2bf4203e9818c4b9cad0ec75c5406d40a0
MaqsudQurbonov <114643709+MaqsudQurbonov@users.noreply.github.com>
2024-04-10 20:34:02 +0500

    L_9

---
+++ L_9.py
@@ -0,0 +1,41 @@
+# print("n dan m gacha 3 ga qoldiqsiz bolinuvchi sonlar")
+# n=int(input("n: "))
+# m=int(input("m: "))
+# while n<=m:
+#     if n % 3 ==0:
+#         print(n, end=" ")
+#     n+=1
+# n = int(input("n:"))
+# i = 1
+# k = 0
+# while i <= n:
+#     if n % i == 0:
+#         k+=1
+#     i += 1
+#     if k > 2:
+#         break
+# if k == 2:
+#     print(f"{n} tub son")
+# else:
+#     print(f"{n} tub son emas")
+
+
+print("2 ta sonning EKUBINI topuvchi dastur tuzing.")
+# a = int(input("a sonini kiriting: "))
+# b = int(input("b sonini kiriting: "))
+# while a!=b:
+#     if a>b:
+#         a-=b
+#     else:
+#         b-=a
+# print(f"EKUBI: {a}")
+
+# a = int(input("a="))
+# b = int(input("b="))
+# while a !=0 and b !=0:
+#     if a>b:
+#         a %=b
+#     else:
+#         b %=a
+# c = a + b
+# print(c)

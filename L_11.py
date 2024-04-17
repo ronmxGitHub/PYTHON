7ab23573b40670479c3a162bbabc7f1e83d7d276
Maksudbek Qurbonov <166323797+ronmxGitHub@users.noreply.github.com>
2024-04-17 21:23:52 +0500

    O'z

--- L_11.py
+++ L_11.py
@@ -25,41 +25,41 @@
 
 #\\\\\\\\Arifmetik amalni bajaruvchi funksiya (2-usul)////////////
 
-# def kalkulyator(belgi,*numbers):
-#     kop = 1
-#     ayirma = numbers[0]
-#     bolinma = numbers[0]
-#     if belgi == '+':
-#         return sum(numbers)
-#     elif belgi == '*':
-#         for n in numbers:
-#             kop *= n
-#         return kop
-#     elif belgi == '-':
-#         for n in numbers:
-#             ayirma -= n
-#         return ayirma + numbers[0]
-#     elif belgi == '/':
-#         for n in numbers:
-#             bolinma /= n
-#         return bolinma * numbers[0]
-#     else:
-#         result = "Iltimos! quyidagi belgilardan bittasini tanlang:('+','-','*','/')"
-#         return result
+def amal(belgi,*numbers):
+    kop = 1
+    ayirma = numbers[0]
+    bolinma = numbers[0]
+    if belgi == '+':
+        return sum(numbers)
+    elif belgi == '*':
+        for n in numbers:
+            kop *= n
+        return kop
+    elif belgi == '-':
+        for n in numbers:
+            ayirma -= n
+        return ayirma + numbers[0]
+    elif belgi == '/':
+        for n in numbers:
+            bolinma /= n
+        return bolinma * numbers[0]
+    else:
+        result = "Iltimos! quyidagi belgilardan bittasini tanlang:('+','-','*','/')"
+        return result
+
+belgi = input("('+', '-', '*', '/') shulardan bittasini tanlang!:")
+print(amal(belgi,4,5,8))
+
+# def birlashtir(*args):
+#     birlashgan_royxat = []
+#     for royxat in args:
+#         birlashgan_royxat.extend(royxat)
+#     return birlashgan_royxat
+# royxat1 = [1,2,3]
+# royxat2 = [4,5,6]
+# royxat3 = [7,8,9]
+# birlashgan = birlashtir(royxat1,royxat2,royxat3)
+# print(birlashgan)
 #
-# belgi = input("('+', '-', '*', '/') shulardan bittasini tanlang!:")
-# print(kalkulyator(belgi,6,9,3))
-
-def birlashtir(*args):
-    birlashgan_royxat = []
-    for royxat in args:
-        birlashgan_royxat.extend(royxat)
-    return birlashgan_royxat
-royxat1 = [1,2,3]
-royxat2 = [4,5,6]
-royxat3 = [7,8,9]
-birlashgan = birlashtir(royxat1,royxat2,royxat3)
-print(birlashgan)
-
-lits = [*royxat1, *royxat2, *royxat3]
-print(lits)
+# lits = [*royxat1, *royxat2, *royxat3]
+# print(lits)

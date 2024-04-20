dd9afcbda801e15ba848fb0a32b1672369314c95
Maksudbek Qurbonov <166323797+ronmxGitHub@users.noreply.github.com>
2024-04-20 08:05:11 +0500

    d

--- L_12.py
+++ L_12.py
@@ -3,3 +3,12 @@ son = list(filter(lambda x : x % 2 == 0,sonlar))
 print(son)
 kvadrat = list(map(lambda x : x * x, son))
 print(kvadrat)
+
+# numbers = list(range(-5,5))
+# print(numbers)
+# musbat_numbers = list(filter(lambda num: num > 0,numbers))
+# print(musbat_numbers)
+
+numbers = list(range(1,6))
+teskari_list = lambda num : num[::-1]
+print(teskari_list(numbers))

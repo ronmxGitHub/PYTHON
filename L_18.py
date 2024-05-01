ef29e2d9935a24edd3a7cefec4ced6a7c7667fcf
Maksudbek Qurbonov <166323797+ronmxGitHub@users.noreply.github.com>
2024-05-02 01:19:05 +0500

    18uv

---
+++ L_18.py
@@ -0,0 +1,17 @@
+import json
+# data = {"Model" : "Malibu",
+#         "Rang" : "Qora",
+#         "Yil":2020,
+#         "Narh":40000}
+# data_json=json.dumps(data,indent=4)
+# print(data_json)
+
+
+
+talaba_json = {"ism":"Hasan",
+               "familiya":"Husanov",
+               "tyil":2000}
+
+a=talaba_json["ism"]
+b=talaba_json["familiya"]
+print(f"{a} {b}")
